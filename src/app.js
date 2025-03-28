16cc3cc4ba8a49ed28bbaa73b968fcd090191d3f
unknown <gufrankhursheed7@gmail.com>
2025-03-28 15:52:13 +0530

    add user model

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser";
 
 const app = express()
+
+app.use(cors())
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 export default app
